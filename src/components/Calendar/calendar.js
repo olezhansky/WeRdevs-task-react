9e972987c22cc80745b4dd02e022329415ed9d17
olezhansky <fenix12895@gmail.com>
2021-08-28 17:50:44 +0300

    commit

--- src/components/Calendar/calendar.js
+++ src/components/Calendar/calendar.js
@@ -1,9 +1,6 @@
 const DAYS_IN_WEEK = 7;
-
 const DAYS_IN_MONTH = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-
 const WEEK_DAYS_FROM_MONDAY = [0, 1, 2, 3, 4, 5, 6];
-
 const monthFebruary = 1;
 
 export function isLeapYear(year) {
@@ -24,7 +21,6 @@ export function getDaysInMonth(date) {
 
 export function getDayOfWeek(date) {
   const dayOfWeek = date.getDay();
-
   return WEEK_DAYS_FROM_MONDAY[dayOfWeek];
 }
 
